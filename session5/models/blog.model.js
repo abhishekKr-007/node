adb06da32bc5a5036b7eba16e9ee242fdecade06
Aditya Singh <106972852+aditya-docs@users.noreply.github.com>
2024-11-16 17:30:20 +0000

    schema options

--- session5/models/blog.model.js
+++ session5/models/blog.model.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-  title: { type: String, required: true, unique: true },
-  authors: { type: [String], default: [] },
-  content: { type: String, default: "" },
-  publishedAt: { type: Date, default: null },
-});
+const blogSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    authors: { type: [String], default: [] },
+    content: { type: String, default: "" },
+    publishedAt: { type: Date, default: null },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
 
 const blogModel = mongoose.model("Blog", blogSchema, "blogs"); //collection name is optional, if not provided, it will default to plural form of the model name
 
