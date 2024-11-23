37cdfb5c5252549f0323606b789c266c04e91ce7
Aditya Singh <106972852+aditya-docs@users.noreply.github.com>
2024-11-23 22:58:15 +0530

    signup and login routes

--- session8-9-10/controllers/auth.controller.js
+++ session8-9-10/controllers/auth.controller.js
@@ -1,4 +1,47 @@
-const postSignup = (req, res) => {};
-const postLogin = (req, res) => {};
+const AuthService = require("../services/auth.service");
+const UserService = require("../services/user.service");
+const AuthServiceInstance = new AuthService();
+const UserServiceInstance = new UserService();
+
+const postSignup = async (req, res) => {
+  try {
+    const newUser = await AuthServiceInstance.signup(req.body);
+    res.status(201).send(newUser);
+  } catch (error) {
+    if (error.code === 11000)
+      return res
+        .status(409)
+        .send({ message: `User with this email already exists.` });
+    res.status(500).send({ message: `Failed to sign user up!` });
+  }
+};
+
+const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const reqUser = await UserServiceInstance.findByUsername(username);
+    if (!reqUser)
+      return res.status(404).send({
+        message: `User with username: ${username} could not be found!`,
+      });
+
+    const isLoggedIn = await AuthServiceInstance.comparePassword(
+      password,
+      reqUser.password
+    );
+    if (!isLoggedIn) return res.status(401).send({ isLoggedIn });
+    res
+      .header(
+        "remember-user-token",
+        AuthServiceInstance.generateJwt({ userId: reqUser._id })
+      )
+      .send({
+        isLoggedIn,
+      });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: `Failed to login :( Try again!` });
+  }
+};
 
 module.exports = { postSignup, postLogin };
---
+++ session8-9-10/services/auth.service.js
@@ -0,0 +1,21 @@
+const bcrypt = require("bcrypt");
+const Jwt = require("jsonwebtoken");
+const User = require("../models/user.model");
+
+class AuthService {
+  signup = async (payload) =>
+    User.create({
+      ...payload,
+      password: await this.generatePasswordHash(payload.password),
+    });
+
+  generatePasswordHash = (password) => bcrypt.hash(password, 10);
+
+  comparePassword = (plainTextPassword, hashedPassword) =>
+    bcrypt.compare(plainTextPassword, hashedPassword);
+
+  generateJwt = (payload) =>
+    Jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: "20s" });
+}
+
+module.exports = AuthService;
